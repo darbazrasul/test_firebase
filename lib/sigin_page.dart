f66843670f1fd64eddaa9bf561b46b0e665357b7
darbaz dev <110346010+darbazrasul@users.noreply.github.com>
2024-06-13 07:52:07 +0300

    get data

--- lib/sigin_page.dart
+++ lib/sigin_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ReadUp extends StatefulWidget {
   const ReadUp({super.key});
@@ -11,11 +12,13 @@ class ReadUp extends StatefulWidget {
 class _ReadUpState extends State<ReadUp> {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
   late CollectionReference users;
+  var data;
 
   @override
   void initState() {
     super.initState();
     users = _db.collection('users');
+    data = users.doc('18nc3kXIE5TtjVg1x93J').get();
   }
 
   Future<void> addUser() async {
@@ -33,19 +36,17 @@ class _ReadUpState extends State<ReadUp> {
     }
   }
 
-   
-
   Future<void> setUser() async {
     try {
       await users.doc('18nc3kXIE5TtjVg1x93J').set({
         'user': 'navid',
       });
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('User added successfully')),
+        const SnackBar(content: Text('User set successfully')),
       );
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to add user: $e')),
+        SnackBar(content: Text('Failed to set user: $e')),
       );
     }
   }
@@ -65,22 +66,83 @@ class _ReadUpState extends State<ReadUp> {
     }
   }
 
+  Future fetchUser() async {
+    setState(() {
+      data = users.doc('18nc3kXIE5TtjVg1x93J').get();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                fetchUser();
+              });
+            },
+            icon: Icon(Icons.refresh),
+          ),
+        ],
         title: const Text('ReadUp App'),
       ),
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: TextButton(
-            onPressed: () async {
-              await updateUser();
-            },
-            child: const Text('Add User'),
-          ),
+      body: RefreshIndicator(
+        onRefresh: fetchUser,
+        child: ListView(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 100,
+              color: Colors.amber,
+              child: FutureBuilder<DocumentSnapshot>(
+                future: data,
+                builder: (BuildContext context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    Map<String, dynamic> data =
+                        snapshot.data!.data() as Map<String, dynamic>;
+                    return Text(
+                      "User: ${data['user']}",
+                      style: TextStyle(color: Colors.black, fontSize: 40),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return const Text("Something went wrong");
+                  }
+
+                  if (snapshot.hasData && !snapshot.data!.exists) {
+                    return const Text("Document does not exist");
+                  }
+
+                  return Text(' ');
+                },
+              ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: addUser,
+            tooltip: 'Add User',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: setUser,
+            tooltip: 'Set User',
+            child: const Icon(Icons.edit),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: updateUser,
+            tooltip: 'Update User',
+            child: const Icon(Icons.update),
+          ),
+        ],
       ),
     );
   }
